3aefd2e71fe3303b9ea7c41a34bd4de19890c452
chinmayGhadigaonka <ghadigaonkarchinmay@gmail.com>
2024-07-11 21:47:51 +0530

    cors

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 dotenv.config();
 
 const corsOptions = {
-  origin: ["*"],
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, //access-control-allow-credentials:true
